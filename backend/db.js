fb71235d2d3fd2462860b7a67bb4868f6355ff8a
zoolake <junhomoon224@gmail.com>
2022-06-11 00:24:44 +0900

    travis ci deploy

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "1234",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 exports.pool = pool;
